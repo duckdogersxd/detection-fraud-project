7ba157e85736595a8bd0bcd383a8cb41f2dc11ce
Faranha300 <fabricioaranha1@gmail.com>
2025-12-16 17:30:30 -0300

    feat: Comparação de Resultados

--- models/isolation_forest.py
+++ models/isolation_forest.py
@@ -1,10 +1,10 @@
 from sklearn.ensemble import IsolationForest
 
 def isoforest(x_train, x_test, estimators, contamination, max_samples):
-    iso_forest = IsolationForest(n_estimators=estimators, contamination=contamination, max_samples=max_samples, random_state=42)
+    model = IsolationForest(n_estimators=estimators, contamination=contamination, max_samples=max_samples, random_state=42)
     
-    iso_forest.fit(x_train)
-    y_pred_raw = iso_forest.predict(x_test)
+    model.fit(x_train)
+    y_pred_raw = model.predict(x_test)
     y_pred = [1 if x == -1 else 0 for x in y_pred_raw]
     
-    return iso_forest, y_pred
+    return model, y_pred
